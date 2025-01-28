2f46839c98455a0b999f0c181835c91ed2a922ab
kjmse716 <kjmse716@gmail.com>
2025-01-28 16:54:26 +0800

    commit

---
+++ LAB1B/Debug/driverlib/MSP430FR2xx_4xx/ecomp.d
@@ -0,0 +1,60 @@
+# FIXED
+
+driverlib/MSP430FR2xx_4xx/ecomp.obj: ../driverlib/MSP430FR2xx_4xx/ecomp.c
+driverlib/MSP430FR2xx_4xx/ecomp.obj: ../driverlib/MSP430FR2xx_4xx/inc/hw_memmap.h
+driverlib/MSP430FR2xx_4xx/ecomp.obj: E:/Programs/CCS_theia/ccs/ccs_base/msp430/include/msp430.h
+driverlib/MSP430FR2xx_4xx/ecomp.obj: E:/Programs/CCS_theia/ccs/ccs_base/msp430/include/msp430fr4133.h
+driverlib/MSP430FR2xx_4xx/ecomp.obj: E:/Programs/CCS_theia/ccs/ccs_base/msp430/include/in430.h
+driverlib/MSP430FR2xx_4xx/ecomp.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+driverlib/MSP430FR2xx_4xx/ecomp.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+driverlib/MSP430FR2xx_4xx/ecomp.obj: E:/Programs/CCS_theia/ccs/ccs_base/msp430/include/msp430fr2xx_4xxgeneric.h
+driverlib/MSP430FR2xx_4xx/ecomp.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h
+driverlib/MSP430FR2xx_4xx/ecomp.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h
+driverlib/MSP430FR2xx_4xx/ecomp.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h
+driverlib/MSP430FR2xx_4xx/ecomp.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_stdint40.h
+driverlib/MSP430FR2xx_4xx/ecomp.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h
+driverlib/MSP430FR2xx_4xx/ecomp.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h
+driverlib/MSP430FR2xx_4xx/ecomp.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h
+driverlib/MSP430FR2xx_4xx/ecomp.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h
+driverlib/MSP430FR2xx_4xx/ecomp.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h
+driverlib/MSP430FR2xx_4xx/ecomp.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h
+driverlib/MSP430FR2xx_4xx/ecomp.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h
+
+../driverlib/MSP430FR2xx_4xx/ecomp.c:
+
+../driverlib/MSP430FR2xx_4xx/inc/hw_memmap.h:
+
+E:/Programs/CCS_theia/ccs/ccs_base/msp430/include/msp430.h:
+
+E:/Programs/CCS_theia/ccs/ccs_base/msp430/include/msp430fr4133.h:
+
+E:/Programs/CCS_theia/ccs/ccs_base/msp430/include/in430.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+E:/Programs/CCS_theia/ccs/ccs_base/msp430/include/msp430fr2xx_4xxgeneric.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_stdint40.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h:
+
---
+++ LAB1B/Debug/main.d
@@ -0,0 +1,138 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/driverlib.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/inc/hw_memmap.h
+main.obj: E:/Programs/CCS_theia/ccs/ccs_base/msp430/include/msp430.h
+main.obj: E:/Programs/CCS_theia/ccs/ccs_base/msp430/include/msp430fr4133.h
+main.obj: E:/Programs/CCS_theia/ccs/ccs_base/msp430/include/in430.h
+main.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+main.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+main.obj: E:/Programs/CCS_theia/ccs/ccs_base/msp430/include/msp430fr2xx_4xxgeneric.h
+main.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h
+main.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h
+main.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h
+main.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_stdint40.h
+main.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h
+main.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h
+main.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h
+main.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h
+main.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h
+main.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h
+main.obj: E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/rom_driverlib.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/rom_map_driverlib.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/lcd_e.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/eusci_b_i2c.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/ecomp.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/cs.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/sac.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/timer_b.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/framctl.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/eusci_a_spi.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/eusci_a_uart.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/tia.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/tlv.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/timer_a.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/pmm.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/sysctl.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/sfr.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/mpy32.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/wdt_a.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/icc.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/gpio.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/eusci_b_spi.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/adc.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/crc.h
+main.obj: E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/rtc.h
+
+../main.c:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/driverlib.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/inc/hw_memmap.h:
+
+E:/Programs/CCS_theia/ccs/ccs_base/msp430/include/msp430.h:
+
+E:/Programs/CCS_theia/ccs/ccs_base/msp430/include/msp430fr4133.h:
+
+E:/Programs/CCS_theia/ccs/ccs_base/msp430/include/in430.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+E:/Programs/CCS_theia/ccs/ccs_base/msp430/include/msp430fr2xx_4xxgeneric.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_stdint40.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h:
+
+E:/Programs/CCS_theia/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/rom_driverlib.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/rom_map_driverlib.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/lcd_e.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/eusci_b_i2c.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/ecomp.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/cs.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/sac.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/timer_b.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/framctl.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/eusci_a_spi.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/eusci_a_uart.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/tia.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/tlv.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/timer_a.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/pmm.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/sysctl.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/sfr.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/mpy32.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/wdt_a.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/icc.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/gpio.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/eusci_b_spi.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/adc.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/crc.h:
+
+E:/Lab/Memory/LAB1/LAB1/LAB1B/driverlib/MSP430FR2xx_4xx/rtc.h:
+
